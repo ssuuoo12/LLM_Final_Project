ad272ed7bde6393e65ade73d67cdf25bef836d1d
SK-Kwon90 <ssoonkyu90@gmail.com>
2025-04-03 12:13:14 +0900

    initial commit: 프로젝트 폴더, FastAPI, spring 구성완료

---
+++ spring_app/src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,9 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    // 추가적인 MVC 설정이 필요하면 여기에 작성
+}
